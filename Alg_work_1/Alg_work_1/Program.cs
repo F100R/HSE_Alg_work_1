9064ae69f1cf5934660a6bdaaf43c774c5014351
F100R <k0pyt0v.s.v@yandex.ru>
2018-09-13 13:55:39 +0500

    Простая подстановка

--- Alg_work_1/Alg_work_1/Program.cs
+++ Alg_work_1/Alg_work_1/Program.cs
@@ -10,7 +10,20 @@ namespace Alg_work_1
     {
         static void Main(string[] args)
         {
-            //Тест №1
+            //Простой вызов
+           int X0 = 0;
+            int X1 = 0;
+            int Xn = 1;
+            int N = 3;
+
+            for (int i = 0; i < N+1; i++)
+            {
+                X0 = X1;
+                X1 = Xn;
+                Xn=12-2*X0+3*X1;
+
+            }
+            Console.WriteLine(X0);
         }
     }
 }
